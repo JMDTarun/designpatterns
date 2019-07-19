8325b894c5c62d3802fa34f2ae9aac2e8412a061
Rohit <rohit.kumar@lab49.com>
2019-07-19 15:24:56 +0530

    WIP Fastway

--- src/main/java/com/user/mngmnt/driver/DriverConfig.java
+++ src/main/java/com/user/mngmnt/driver/DriverConfig.java
@@ -38,8 +38,8 @@ public class DriverConfig {
     // @Bean
     public WebDriver driver() {
 
-      System.setProperty("webdriver.chrome.driver", driverFile.getAbsolutePath());
-      //System.setProperty("webdriver.chrome.driver", "/Users/rohitsrivastava/Codebase/user-management-master/src/main/resources/chromedriver");
+      //System.setProperty("webdriver.chrome.driver", driverFile.getAbsolutePath());
+      System.setProperty("webdriver.chrome.driver", "/Users/lab/Desktop/user_management/src/main/resources/chromedriver");
 
       ChromeOptions chromeOptions = new ChromeOptions();
       //chromeOptions.addArguments("headless");
--- src/main/java/com/user/mngmnt/driver/FastwayRunner.java
+++ src/main/java/com/user/mngmnt/driver/FastwayRunner.java
@@ -7,12 +7,16 @@ import com.user.mngmnt.model.RunnerExecution;
 import com.user.mngmnt.model.RunnerExecutionStatus;
 import com.user.mngmnt.repository.RunnerExecutionRepository;
 import org.apache.commons.collections4.CollectionUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -31,104 +35,126 @@ import static com.github.loyada.jdollarx.ElementProperties.hasName;
 import static com.github.loyada.jdollarx.ElementProperties.hasParent;
 import static com.github.loyada.jdollarx.ElementProperties.hasText;
 import static com.github.loyada.jdollarx.ElementProperties.isNthSibling;
+import static com.github.loyada.jdollarx.Operations.*;
 import static com.github.loyada.jdollarx.singlebrowser.InBrowserSinglton.clickAt;
 import static com.github.loyada.jdollarx.singlebrowser.InBrowserSinglton.driver;
 import static com.github.loyada.jdollarx.singlebrowser.InBrowserSinglton.sendKeys;
 import static java.util.concurrent.TimeUnit.*;
 
 @Component
-public class FastwayRunner{
+public class FastwayRunner {
 
-  public static final String URL = "http://bssnew.myfastway.in:9003/oapservice";
-  public static final String USERNAME = "KAMALJIT.SINGH";
-  public static final String PASSWORD = "Kamal@123";
+    public static final String URL = "http://bssnew.myfastway.in:9003/oapservice";
+    public static final String USERNAME = "KAMALJIT.SINGH";
+    public static final String PASSWORD = "Kamal@123";
 
+    @Autowired
+    private DriverConfig driverConfig;
 
-  @Autowired
-  private DriverConfig driverConfig;
+    @Autowired
+    private RunnerExecutionRepository runnerExecutionRepository;
 
-  @Autowired
-  private RunnerExecutionRepository runnerExecutionRepository;
+    public void run() {
+        // List<RunnerExecution> executions = runnerExecutionRepository.findByStatus(RunnerExecutionStatus.IN_PROGRESS);
+        List<RunnerExecution> executions = new ArrayList<>();
+        if (CollectionUtils.isNotEmpty(executions)) {
+            throw new RuntimeException("A instance of runner execution is already in progress.");
+        }
 
-  public void run(){
+        RunnerExecution currentExcution = runnerExecutionRepository.save(RunnerExecution.builder()
+                .status(RunnerExecutionStatus.IN_PROGRESS)
+                .startTime(Instant.now())
+                .build());
+        try {
 
-    List<RunnerExecution> executions = runnerExecutionRepository.findByStatus(RunnerExecutionStatus.IN_PROGRESS);
+            //Get action performed data till time that are not processed
 
-    if(CollectionUtils.isNotEmpty(executions)){
-      throw new RuntimeException("A instance of runner execution is already in progress.");
+            login();
+            searchDevice("56331345071201");
+            addPack("HD");
+
+            //On execution complete
+            currentExcution.setEndTime(Instant.now());
+            currentExcution.setStatus(RunnerExecutionStatus.COMPLETED);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            currentExcution.setStatus(RunnerExecutionStatus.ERROR);
+            currentExcution.setErrorMsg(e.getMessage());
+        } finally {
+            runnerExecutionRepository.save(currentExcution);
+            //close();
+        }
     }
 
-    RunnerExecution currentExcution = runnerExecutionRepository.save(RunnerExecution.builder()
-      .status(RunnerExecutionStatus.IN_PROGRESS)
-      .startTime(Instant.now())
-    .build());
-    try {
-
-      //Get action performed data till time that are not processed
-
-      login();
-      searchDevice("56331345071201");
-      addPack("HD");
-
-      //On execution complete
-      currentExcution.setEndTime(Instant.now());
-      currentExcution.setStatus(RunnerExecutionStatus.COMPLETED);
-
+    private boolean login() throws IOException, URISyntaxException {
+        driver = driverConfig.driver();
+        driver.manage().timeouts().implicitlyWait(60, SECONDS).pageLoadTimeout(60, SECONDS);
+        driver.get(URL);
+        sendKeys(USERNAME).to(input.that(hasName("username")));
+        sendKeys(PASSWORD).to(input.that(hasName("password")));
+        clickAt(input.that(hasAttribute("type", "submit")));
+        return true;
     }
-    catch (Exception e){
-      e.printStackTrace();
-      currentExcution.setStatus(RunnerExecutionStatus.ERROR);
-      currentExcution.setErrorMsg(e.getMessage());
+
+    private boolean searchDevice(String serialNumber) throws OperationFailedException, InterruptedException {
+        doWithRetries(() -> {
+            try {
+                Path searchSection = div.withClass("summary_search");
+                sendKeys("serialno").to(select.withClass("inner_custom").inside(searchSection));
+                sendKeys(serialNumber).to(input.withClass("nav-search-input").inside(searchSection));
+                clickAt(button.withClass("btn btn-sm btn-danger btn-round").inside(searchSection));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, 60, 1000);
+        return true;
     }
-    finally {
-      runnerExecutionRepository.save(currentExcution);
-      //close();
+
+
+    private boolean addPack(String name) throws InterruptedException, OperationFailedException, Exception {
+        // Thread.sleep(3000);
+        doWithRetries(() ->
+                        clickAt(button.that(hasText("Show Details")
+                                .and(hasAncesctor(tr.that(isNthSibling(0)
+                                        .and(hasAncesctor(table.that(hasId("activeservice")))))))))
+                , 60, 1000);
+//    clickAt(button.that(hasText("Show Details")
+//            .and(hasAncesctor(tr.that(isNthSibling(0)
+//                    .and(hasAncesctor(table.that(hasId("activeservice")))))))));
+//    Thread.sleep(3000);
+        doWithRetries(() ->
+                        clickAt(button.that(hasText("Add Plan").and(hasAncesctor(div.that(hasId("activeservice1"))))))
+                , 60, 1000);
+//    Thread.sleep(3000);
+
+        doWithRetries(() -> {
+                    Path addPlanModal = div.that(hasId("add_change_plandialog"));
+                    try {
+                        sendKeys("SUGGESTIVE PACKS").to(select.that(hasName("subscription-plan-list-name").and(hasAncesctor(addPlanModal))));
+                        sendKeys("Recovery").to(select.that(hasName("subscription-plan-list-select-reason")).inside(addPlanModal));
+                    } catch (OperationFailedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                , 60, 1000);
+
+//    Thread.sleep(3000);
+
+
+//    Thread.sleep(1000);
+        doWithRetries(() -> {
+                    Path addPlanModal = div.that(hasId("add_change_plandialog"));
+                    clickAt(button.that(hasAggregatedTextContaining("Submit")).inside(addPlanModal));
+                }
+                , 60, 1000);
+
+        return true;
     }
-  }
 
-  private boolean login() throws IOException, URISyntaxException {
-    driver = driverConfig.driver();
-    driver.manage().timeouts().implicitlyWait(60, SECONDS).pageLoadTimeout(60, SECONDS);
-    driver.get(URL);
-    sendKeys(USERNAME).to(input.that(hasName("username")));
-    sendKeys(PASSWORD).to(input.that(hasName("password")));
-    clickAt(input.that(hasAttribute("type", "submit")));
-    return true;
-  }
-
-  private boolean searchDevice(String serialNumber) throws Operations.OperationFailedException, InterruptedException {
-    Thread.sleep(3000);
-    Path searchSection = div.withClass("summary_search");
-    sendKeys("serialno").to(select.withClass("inner_custom").inside(searchSection));
-    sendKeys(serialNumber).to(input.withClass("nav-search-input").inside(searchSection));
-    clickAt(button.withClass("btn btn-sm btn-danger btn-round").inside(searchSection));
-    return true;
-  }
-
-
-  private boolean addPack(String name) throws InterruptedException, Operations.OperationFailedException {
-    Thread.sleep(3000);
-    clickAt(button.that(hasText("Show Details")
-            .and(hasAncesctor(tr.that(isNthSibling(0)
-                    .and(hasAncesctor(table.that(hasId("activeservice")))))))));
-    Thread.sleep(3000);
-    clickAt(button.that(hasText("Add Plan").and(hasAncesctor(div.that(hasId("activeservice1"))))));
-    Thread.sleep(3000);
-    Path addPlanModal = div.that(hasId("add_change_plandialog"));
-    sendKeys("SUGGESTIVE PACKS").to(select.that(hasName("subscription-plan-list-name").and(hasAncesctor(addPlanModal))));
-    sendKeys("Recovery").to(select.that(hasName("subscription-plan-list-select-reason")).inside(addPlanModal));
-    Thread.sleep(3000);
-    clickAt(td.that(hasAggregatedTextContaining(name)).inside(table.that(hasId("subscription-plan-details")).inside(addPlanModal)));
-    Thread.sleep(1000);
-    clickAt(button.that(hasAggregatedTextContaining("Submit")).inside(addPlanModal));
-    return true;
-  }
-
-  private void close(){
-    driver.quit();
-  }
-
-
+    private void close() {
+        driver.quit();
+    }
 
 
 }
